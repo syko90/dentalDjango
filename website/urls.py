4644907df5d4e43e9f6b0e304e804939f275b96e
psycho <kurantowicz.maciej@gmail.com>
2020-02-21 07:25:15 +0100

    To ma być ok1

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact.html', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'), # doatkowa opcja
 ]
